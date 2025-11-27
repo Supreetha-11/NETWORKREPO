628e66fb1d74fee75531c4b60f7b2f1410725487
HP <HP@DESKTOP-K32KFOQ.mshome.net>
2025-11-27 16:07:06 +0530

    UPDATE

---
+++ network/src/test/java/sampletest/javatest.java
@@ -0,0 +1,17 @@
+package sampletest;
+
+import org.testng.annotations.Test;
+
+public class javatest {
+	@Test
+	public void suptest()
+	{
+		System.out.println("done");
+		System.out.println("HI");
+		System.out.println("BYE");
+		System.out.println("WHY SDF");
+		
+	}
+
+
+}
